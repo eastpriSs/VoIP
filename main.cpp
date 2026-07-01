2d6a081cdec4202059ff48f5ed0d555302b84e51
eastpriss <vioscamo8@gmail.com>
2026-07-01 13:15:22 +0700

    Added virtual keyboard

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 #include <opus/opus.h>
 #include <pjsua2.hpp>
 #include <QApplication>
+#include <QLocale>
 
 int main(int argc, char *argv[])
-{ 
+{
+    QLocale::setDefault(QLocale("en_US"));
+    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+
+    // Принудительно используем X11/Xwayland вместо Wayland для Fedora 40
+    qputenv("QT_QPA_PLATFORM", QByteArray("xcb"));
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
